11d71067604204357695a837df654f6ab234e9ff
Gabor Makrai <makraigabor@gmail.com>
2014-12-08 16:30:01 +0000

    NeighbourArrayGraphGeneratorTest: has test for BaseDijkstra

--- DijkstraPerformance/test/dijkstra/graph/NeighbourArrayGraphGeneratorTest.java
+++ DijkstraPerformance/test/dijkstra/graph/NeighbourArrayGraphGeneratorTest.java
@@ -6,6 +6,8 @@ import java.util.Random;
 
 import org.junit.Test;
 
+import dijkstra.base.BaseDijkstra;
+
 public class NeighbourArrayGraphGeneratorTest {
 	
 	@Test
@@ -23,6 +25,35 @@ public class NeighbourArrayGraphGeneratorTest {
 		
 		assertTrue(generator.neighbours != null);
 		assertEquals(18, arcs);
+	}
+	
+	@Test
+	public void testDijkstra() {
+		NeighbourArrayGraphGenerator generator = new NeighbourArrayGraphGenerator();
+		generator.generateRandomGraph(10, 0, new Random(42));
+		
+		int[] previous = new int[10];
+		double[] distance = new double[10];
+		
+		for (int origin = 0; origin < 10; ++origin) {
+			BaseDijkstra.createPreviousArray(generator.neighbours, generator.weights, origin, distance, previous);
+			boolean valid = true;
+			for (int i = 0; i < 10; ++i) {
+				if (i == origin) {
+					if (previous[i] != -1) {
+						valid = false;
+						break;
+					}
+					continue;
+				}
+				if (previous[i] == -1) {
+					valid = false;
+					break;
+				}
+			}
+			assertTrue(valid);
+		}
+		
 	}
 	
 }
