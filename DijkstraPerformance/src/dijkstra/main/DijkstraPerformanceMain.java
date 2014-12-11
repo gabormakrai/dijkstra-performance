6e84fddfa6fce3a62c4932f5d4177d79b4ef71d1
gabormakrai <makraigabor@gmail.com>
2014-12-11 23:52:37 +0000

    contains PerformanceScanrio for Nutch's FH PQ and adding it to the main program

--- DijkstraPerformance/src/dijkstra/main/DijkstraPerformanceMain.java
+++ DijkstraPerformance/src/dijkstra/main/DijkstraPerformanceMain.java
@@ -4,6 +4,7 @@ import dijkstra.performance.PerformanceEngine;
 import dijkstra.performance.PerformanceScenario;
 import dijkstra.performance.scenario.RandomBaseScenario;
 import dijkstra.performance.scenario.RandomNeo4jFibonacciPriorityQueueScenario;
+import dijkstra.performance.scenario.RandomNutchFibonacciPriorityQueueScenario;
 import dijkstra.performance.scenario.RandomTreeSetPriorityQueueScenario;
 
 public class DijkstraPerformanceMain {
@@ -12,6 +13,7 @@ public class DijkstraPerformanceMain {
 		PerformanceScenario scenarioBase = new RandomBaseScenario(1000, 100000, 20, 42);
 		PerformanceScenario scenarioPriorityQueue = new RandomTreeSetPriorityQueueScenario(1000, 100000, 20, 42);
 		PerformanceScenario scenarioNeo4jFibonacciPriorityQueue = new RandomNeo4jFibonacciPriorityQueueScenario(1000, 100000, 20, 42);
+		PerformanceScenario scenarioNutchFibonacciPriorityQueue = new RandomNutchFibonacciPriorityQueueScenario(1000, 100000, 20, 42);
 	
 		PerformanceEngine engine = new PerformanceEngine(scenarioBase);
 		engine.startMeasurement(20);
@@ -21,5 +23,8 @@ public class DijkstraPerformanceMain {
 		
 		PerformanceEngine engine3 = new PerformanceEngine(scenarioNeo4jFibonacciPriorityQueue);
 		engine3.startMeasurement(20);
+		
+		PerformanceEngine engine4 = new PerformanceEngine(scenarioNutchFibonacciPriorityQueue);
+		engine4.startMeasurement(20);		
 	}
 }
---
+++ DijkstraPerformance/src/dijkstra/performance/scenario/RandomNutchFibonacciPriorityQueueScenario.java
@@ -0,0 +1,52 @@
+package dijkstra.performance.scenario;
+
+import java.util.Random;
+
+import dijkstra.graph.NeighbourArrayGraphGenerator;
+import dijkstra.performance.PerformanceScenario;
+import dijkstra.priority.DijkstraPriorityObject;
+import dijkstra.priority.PriorityQueueDijkstra;
+import dijkstra.priority.impl.NutchFibonacciPriorityQueue;
+
+public class RandomNutchFibonacciPriorityQueueScenario implements PerformanceScenario {
+	
+	NeighbourArrayGraphGenerator generator = new NeighbourArrayGraphGenerator();
+	
+	int[] previous;
+	DijkstraPriorityObject[] priorityObjectArray;
+	NutchFibonacciPriorityQueue priorityQueue;
+	Random random;
+	
+	int randomSeed;
+	int size;
+	int arcs;
+	int previosArrayBuilds;
+	
+	public RandomNutchFibonacciPriorityQueueScenario(int size, int arcs, int previousArrayBuilds, int randomSeed) {
+		this.size = size;
+		this.arcs = arcs;
+		this.previosArrayBuilds = previousArrayBuilds;
+		this.randomSeed = randomSeed;
+	}
+	
+	@Override
+	public void runShortestPath() {
+		for (int i = 0; i < previosArrayBuilds; ++i) {
+			int origin = random.nextInt(size);
+			PriorityQueueDijkstra.createPreviousArray(generator.neighbours, generator.weights, origin, previous, priorityObjectArray, priorityQueue);
+		}
+	}
+	
+	@Override
+	public void generateGraph() {
+		random = new Random(randomSeed);
+		previous = new int[size];
+		generator.generateRandomGraph(size, arcs, random);
+		random = new Random(randomSeed);
+		priorityQueue = new NutchFibonacciPriorityQueue();
+		priorityObjectArray = new DijkstraPriorityObject[size];
+		for (int i = 0; i < size; ++i) {
+			priorityObjectArray[i] = new DijkstraPriorityObject(i, 0.0);
+		}
+	}
+}
