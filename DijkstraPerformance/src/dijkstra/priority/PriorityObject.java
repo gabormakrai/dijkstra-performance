3d0a1b0d6dd75077d9fe4f9249c3224cc268b3c2
Gabor Makrai <makraigabor@gmail.com>
2015-01-03 23:57:17 +0000

    fixes PQ problem

--- DijkstraPerformance/src/dijkstra/priority/PriorityObject.java
+++ DijkstraPerformance/src/dijkstra/priority/PriorityObject.java
@@ -12,12 +12,10 @@ public class PriorityObject implements Comparable<PriorityObject> {
 
 	@Override
 	public int compareTo(PriorityObject o) {
-		if (priority < o.priority) {
-			return -1;
-		} else if (priority > o.priority) {
+		if (priority > o.priority) {
 			return +1;
 		} else {
-			return 0;
+			return -1;
 		}
 	}
 }
